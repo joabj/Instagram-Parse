4133852b74846f0d268efa244d856a79de837baa
Joab Jackson <home@joabj.com>
2019-01-20 22:26:18 -0500

    Write to a file
    Don't understand how Python can print a dictionary but not an entry within the dictionary because of Unicode issues. lolz

--- IG-Parse.py
+++ IG-Parse.py
@@ -6,33 +6,34 @@ from pprint import pprint
 response = open("joabjack.json", 'r')
 data = json.load(response)
 
-#target = open("Instagram.txt", 'w')
+target = open("Instagram.txt", 'w')
 
 x = 0;
 
 while x < 20:
+
 	
-	Earl = (data[x]['display_url'])
-	print str(Earl)
-	print "\r"
-	TimeStamp = data[x]['taken_at_timestamp']
-	print str(TimeStamp)
-	print "\r"
-	Height = data[x]['dimensions']['height']
-	print int(Height)
-	print "\r"	
-	Width = data[x]['dimensions']['width']
-	print int(Width)
-	print "\r"	
-	Caption = data[x]['edge_media_to_caption']['edges']
-	#['node'][u'#text']
-	print str(Caption)
-	print "\r"
-	print "-----------------------------------------------------------------------------------------------"
+	Earl = str(data[x]['display_url'])
+	target.write(Earl)
+	target.write("\r")
+	TimeStamp = str(data[x]['taken_at_timestamp'])
+	target.write("\r")
+	Height = str(data[x]['dimensions']['height'])
+	target.write(Height)
+	target.write("\r")	
+	Width = str(data[x]['dimensions']['width'])
+	target.write(Width)
+	target.write("\r")	
+	Caption = str(data[x]['edge_media_to_caption']["edges"][0]["node"])
+	#['node'][u'text']
+	target.write(Caption)
+	target.write("\r")
+	target.write("-----------------------------------------------------------------------------------------------")
+	target.write("\r")
 	x = x + 1
 	
 	
-#target.close()
+target.close()
 
 #pprint(data)
 #For printing the taxonomy
